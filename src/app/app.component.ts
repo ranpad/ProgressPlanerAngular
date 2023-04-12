5beadaae266402283e27726fbd17f4184cbaa423
ranpad19 <ranpad19@htl-kaindorf.at>
2023-04-12 08:14:18 +0200

    Header + Nav Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
+import {RouterModule} from "@angular/router";
+import {WeightTrackerComponent} from "./weight-tracker/weight-tracker.component";
+import {BodyMeasuresComponent} from "./body-measures/body-measures.component";
+import {CaloricIntakeComponent} from "./caloric-intake/caloric-intake.component";
+import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ProgressPlanerAngular';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {NgOptimizedImage} from "@angular/common";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { WeightTrackerComponent } from './weight-tracker/weight-tracker.component';
+import { BodyMeasuresComponent } from './body-measures/body-measures.component';
+import { CaloricIntakeComponent } from './caloric-intake/caloric-intake.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    WeightTrackerComponent,
+    BodyMeasuresComponent,
+    CaloricIntakeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    NgOptimizedImage,
+    MatSidenavModule,
+    MatListModule,
+    RouterOutlet,
+    AppRoutingModule,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
