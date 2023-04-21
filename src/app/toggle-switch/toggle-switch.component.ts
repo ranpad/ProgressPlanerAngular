8b4698f562115c050ec0f74bec8763095e7dbaba
ranpad19 <ranpad19@htl-kaindorf.at>
2023-04-21 12:56:48 +0200

    Toggle Switch and Weight Chart

---
+++ src/app/toggle-switch/toggle-switch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-switch',
+  templateUrl: './toggle-switch.component.html',
+  styleUrls: ['./toggle-switch.component.css']
+})
+export class ToggleSwitchComponent {
+
+}
