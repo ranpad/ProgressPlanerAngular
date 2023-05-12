7293cbed6d9e042a129bd9cc6c2d0c8f18039e4c
ranpad19 <ranpad19@htl-kaindorf.at>
2023-05-12 16:22:25 +0200

    Post für Login Angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,22 +7,43 @@ import {FormControl, Validators} from "@angular/forms";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
+  inputEmail = new FormControl('', [Validators.required, Validators.email]);
 
   hide = true;
+  email: string | undefined;
+  passwd: string | undefined;
+
+  /*constructor(email: string, passwd: string) {
+    this.email = email;
+    this.passwd = passwd;
+  }*/
+
 
   getErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.inputEmail.hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.inputEmail.hasError('email') ? 'Not a valid email' : '';
   }
 
   login(){
     const url: string = 'http://localhost:8080/api/login';
 
-    fetch(url)
+    const user = {
+      username : this.email,
+      password : this.passwd
+    };
+
+    const init = {
+      method : 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify(user)
+    }
+
+    fetch(url, init)
       .then(res => {
         if (!res.ok){
           throw Error('HTTP-Error: ' + res.status);
